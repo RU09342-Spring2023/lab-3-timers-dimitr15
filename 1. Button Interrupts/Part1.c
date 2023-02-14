c1709aa1f739ef1f4f485eb2ad1afdcb89f71b73
Nikolas Dimitrovski <dimitr15@students.rowan.edu>
2023-02-14 17:18:56 -0500

    part1

--- 1. Button Interrupts/Part1.c
+++ 1. Button Interrupts/Part1.c
@@ -2,7 +2,7 @@
  * Part1.c
  *
  *  Created on: Feb 11, 2023
- *      Author: Russell Trafford
+ *      Author: Modified by Nik Dimitrovski
  *
  *  This code is a template which will change the color of the LED being blinked using the interrupt routine.
  */
@@ -12,7 +12,7 @@
 
 char LED_Color = 0x01;                       // Global Variable to determine which LED should be blinking
 
-void gpioInit()
+void gpioInit();
 
 
 int main(void)
@@ -35,10 +35,10 @@ int main(void)
 
     while(1)
     {
-        if (LEDColor)
+        if (LED_Color)
             P1OUT ^= BIT0;                  // P1.0 = toggle
         else
-            P6OUT &= ~BIT6;                 // Set P1.0 to 0
+            P6OUT ^= BIT6;                 // Set P1.0 to 0
         __delay_cycles(100000);
     }
 }
@@ -80,16 +80,18 @@ __interrupt void Port_2(void)
 {
     P2IFG &= ~BIT3;                         // Clear P1.3 IFG
 
-    if ( )       // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a rising edge.
+    if (P2IES &= ~BIT3)
     {
         LED_Color = 0;
-        // @TODO Add code to change which edge the interrupt should be looking for next
+        P6OUT &= ~BIT6;
+        // Set the edge sensitivity to falling
     }
 
-    else if ( ) // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a falling edge.
+    else
     {
         LED_Color = 1;
-        // @TODO Add code to change which edge the interrupt should be looking for next
+        P1OUT &= ~BIT0;
+        // Set the edge sensitivity to rising
     }
 }
 
