647b5dbefc3b258648b10219b710545391747cef
Nikolas Dimitrovski <dimitr15@students.rowan.edu>
2023-02-20 22:22:17 -0500

    part2

--- 2. Timers/Part2.c
+++ 2. Timers/Part2.c
@@ -2,7 +2,7 @@
  * Part2.c
  *
  *  Created on: Feb 11, 2023
- *      Author: Russell Trafford
+ *      Author: Modified By Nik Dimitrovski
  *
  *      This code will need to change the speed of an LED between 3 different speeds by pressing a button.
  */
@@ -29,30 +29,44 @@ void main(){
 
 
 void gpioInit(){
-    // @TODO Initialize the Red or Green LED
+    // Configure RED LED on P1.0 as Output
+          P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
+          P1DIR |= BIT0;                          // Set P1.0 to output direction
+
+          // Configure Green LED on P6.6 as Output
+          P6OUT &= ~BIT6;                         // Clear P6.6 output latch for a defined power-on state
+          P6DIR |= BIT6;                          // Set P6.6 to output direction
 
     // @TODO Initialize Button 2.3
+            P2OUT &= ~BIT3;
+            P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
+            P2REN |= BIT3;                          // P2.3 pull-up register enable
+            P2IES &= ~BIT3;                         // P2.3 Low --> High edge
+            P2IE |= BIT3;                           // P2.3 interrupt enabled
 
 
 }
 
 void timerInit(){
-    // @TODO Initialize Timer B1 in Continuous Mode using ACLK as the source CLK with Interrupts turned on
+    P1OUT=0X01;   //make bit0 HIGH
+           for(i=0;i<20000;i++){   //    delay till you make LED HIGH
+       }
+           P1OUT=0X00;   //make bit0 LOW
+
+           for(i=0;i<20000;i++){   // delay till you make LED LOW
+                  }
 
 }
-
-
-/*
- * INTERRUPT ROUTINES
- */
 
 // Port 2 interrupt service routine
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
-    // @TODO Remember that when you service the GPIO Interrupt, you need to set the interrupt flag to 0.
+    P1DIR |= 0x41; // Set P1.0 and P1.6 to outputs
 
-    // @TODO When the button is pressed, you can change what the CCR0 Register is for the Timer. You will need to track what speed you should be flashing at.
+    P1REN = 0x08; // Turn on pull up resistor to push button
+
+    P1OUT = 0x49;
 
 }
 
@@ -61,7 +75,10 @@ __interrupt void Port_2(void)
 #pragma vector = TIMER1_B0_VECTOR
 __interrupt void Timer1_B0_ISR(void)
 {
-    // @TODO You can toggle the LED Pin in this routine and if adjust your count in CCR0.
+    P1IE |= BUTTON; // P1.3 interrupt enabled
+
+    P1IFG &= ~BUTTON;
+
 }
 
 
